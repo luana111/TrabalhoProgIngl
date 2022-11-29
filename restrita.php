8a1fd99eae58ec5d4c10e26eac45492944fb792f
AlLeLu3 <118358848+AlLeLu3@users.noreply.github.com>
2022-11-29 15:58:45 -0300

    Update restrita.php

--- restrita.php
+++ restrita.php
@@ -5,9 +5,52 @@ if(!isset($_SESSION['idUsuario'])){
 }
 require_once __DIR__."/vendor/autoload.php";
 
-$contatos = Pessoa::findallByUsuario($_SESSION['idUsuario']);
+$usuaria = Usuario::find($_SESSION['idUsuario']);
+if($usuaria->getadmin()==1){
+?>
+
+<?php
+}else{
 ?>
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Página de Livros</title>
+</head>
+<body>
+<div class="logo_ifrs">
+        <img src="logo_feliz_ifrs.png">
+    </div>
+    <br>
+    <div class="aaa">
+        <h1>LIVROS</h1>
+
+    <table>
+    <tr>
+        <td>Título</td>
+        <td>Autor</td>
+        <td>Genero</td>
+        <td>Ano</td>
+    </tr>
+    <?php
+    foreach($livros as $livro){
+        echo "<tr>";
+        echo "<td>{$livro->getTitulo()}</td>";
+        echo "<td>{$livro->getAutor()}</td>";
+        echo "<td>{$livro->getGenero()}</td>";
+        echo "<td>{$livro->getAno()}</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+<?php
+}
+?>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -32,6 +75,7 @@ $contatos = Pessoa::findallByUsuario($_SESSION['idUsuario']);
         <td>Opções</td>
     </tr>
     <?php
+    /*
     foreach($contatos as $contato){
         echo "<tr>";
         echo "<td>{$contato->getNome()}</td>";
@@ -42,9 +86,9 @@ $contatos = Pessoa::findallByUsuario($_SESSION['idUsuario']);
                 <a href='excluirPessoa.php?id={$contato->getId()}'>Excluir</a> 
              </td>";
         echo "</tr>";
-    }
+    }*/
     ?>
-</table>
+</table>-->
         </main>
         <footer id='page-footer'>
             <h3>Programação 3 - 2022</h3>
@@ -52,5 +96,3 @@ $contatos = Pessoa::findallByUsuario($_SESSION['idUsuario']);
 </div>
 </body>
 </html>
-
-
